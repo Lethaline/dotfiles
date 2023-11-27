b78d87b09e404d6599df05d346836e77e0303f1c
lethaline <lethartiflette@proton.me>
2023-11-27 19:02:04 +0100

    [REFACTOR-0] install.sh refactored

--- install.sh
+++ install.sh
@@ -7,26 +7,28 @@ blue='\033[0;34m'
 
 # Create + Verify symlinks
 
-dotfiles=$(find . -type f -name ".*" -o -name "*.conf" | sort) 
+link_dotfiles()
+{
+	if ln -s "$PWD/$1" "$HOME/$1" ; then
+		echo "[ ${green}OK${nocolor} ] : symlink for $1"
+	else
+		echo "[ ${red}KO${nocolor} ] : an error occured for $1"
+	fi
+}
+
+dotfiles=$(find . -type f -name ".*" -o -name "*.conf" | sort)
 for dotfile in $dotfiles; do
 	correct_path="${dotfile##./}"
-	if [ ! -e "$HOME/$correct_path" ]; then
-		if ln -s "$PWD/$correct_path" "$HOME/$correct_path"; then
-			echo "[ ${green}OK${nocolor} ] : symlink for $correct_path"
-		else
-			echo "[ ${red}KO${nocolor} ] : an error occured for $correct_path"
-		fi
-	else
+	if [ -e "$HOME/$correct_path" ]; then
 		if [ ! $(readlink -f $HOME/$correct_path) = "$PWD/$correct_path" ]; then
 			echo "Bad symlink for $HOME/$correct_path"
 			rm "$HOME/$correct_path"
 			echo "Correction..."
-			if ln -s "$PWD/$correct_path" "$HOME/$correct_path"; then
-				echo "[ ${green}OK${nocolor} ] : new symlink for $correct_path"
-			else
-				echo "[ ${red}KO${nocolor} ] : an error occured for $correct_path"
-			fi
+			link_dotfiles $correct_path
+		else
+			echo "[ ${blue}AOK${nocolor} ] : $correct_path already linked."
 		fi
-		echo "[ ${blue}AOK${nocolor} ] : $correct_path already linked"
-	fi
+	else
+		link_dotfiles $correct_path
+	fi	
 done
